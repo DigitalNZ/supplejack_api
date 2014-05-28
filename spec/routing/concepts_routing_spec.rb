f4b739cd231ef62a68bff1acfd2b1bbd7055d3c3
Benedict Aluan <benedict.aluan@gmail.com>
2014-05-28 15:30:24 +1200

    Added concepts routing spec.

---
+++ spec/routing/concepts_routing_spec.rb
@@ -0,0 +1,22 @@
+# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government, 
+# and is licensed under the GNU General Public License, version 3.
+# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details. 
+# 
+# Supplejack was created by DigitalNZ at the National Library of NZ and 
+# the Department of Internal Affairs. http://digitalnz.org/supplejack
+
+require 'spec_helper'
+
+module SupplejackApi
+  describe 'Concepts route' do
+    routes { SupplejackApi::Engine.routes }
+
+    it 'routes /concepts.format to concepts#index' do
+      expect(get '/concepts.json').to route_to(controller: 'supplejack_api/concepts', action: 'index', format: 'json')
+    end
+
+    # it 'routes /records/1.format to records#index' do
+    #   expect(get '/records/99.json').to route_to(controller: 'supplejack_api/records', action: 'show', id: '99', format: 'json')
+    # end
+  end
+end

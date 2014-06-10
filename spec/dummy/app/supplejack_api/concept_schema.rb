351e82e4659c4bf6daf8fe685d8d03a193ff7baf
Jesse <jessestegmann@gmail.com>
2014-06-10 14:44:25 +1200

    Update namespaces in Concept schema

--- spec/dummy/app/supplejack_api/concept_schema.rb
+++ spec/dummy/app/supplejack_api/concept_schema.rb
@@ -8,44 +8,26 @@
 class ConceptSchema < SupplejackApi::SupplejackSchema
 
   #namespaces
-  namespace :label, url: 'http://www.w3.org/2004/02/skos/core#prefLabel'
-  namespace :foaf, url: 'http://xmlns.com/foaf/0.1/'
+  namespace :skos,   url: 'http://www.w3.org/2004/02/skos/core'
+  namespace :foaf,   url: 'http://xmlns.com/foaf/0.1/'
   namespace :rdaGr2, url: 'http://rdvocab.info/ElementsGr2/'
-  namespace :edm, url: 'http://www.europeana.eu/schemas/edm/'
-  namespace :sameAs, url: 'http://www.w3.org/2002/07/owl#sameA'
-
-  # namespace :description, description: 'rdaGr2:biographicalInformation'
-
+  namespace :edm,    url: 'http://www.europeana.eu/schemas/edm/'
+  namespace :owl,    url: 'http://www.w3.org/2002/07/owl'
 
   # Fields
-<<<<<<< HEAD
-  string    :type,                              search_as: [:filter]
-  string    :name,          search_boost: 10,   search_as: [:filter, :fulltext]
-  string    :label,         search_boost: 5,    search_as: [:filter, :fulltext]
-  string    :description,   search_boost: 2,    search_as: [:filter, :fulltext]
-  datetime  :dateOfBirth,                       search_as: [:filter]
-  datetime  :dateOfDeath,                       search_as: [:filter]
-  string    :placeOfBirth
-  string    :placeOfDeath
-  string    :gender,                            search_as: [:filter]
-  string    :isRelatedTo,   multi_value: true
-  string    :hasMet,        multi_value: true
-  string    :sameAs,        multi_value: true
-=======
-  string    :@id
   string    :@type
   string    :name,          search_boost: 10,   search_as: [:filter, :fulltext], namespace: :foaf
-  string    :label,         search_boost: 5,    search_as: [:filter, :fulltext], namespace: :label
+  string    :label,         search_boost: 5,    search_as: [:filter, :fulltext], namespace: :skos, namespace_field: :prefLabel
   string    :description,   search_boost: 2,    search_as: [:filter, :fulltext], namespace: :rdaGr2, namespace_field: :biographicalInformation
   datetime  :dateOfBirth,   namespace: :rdaGr2
   datetime  :dateOfDeath,   namespace: :rdaGr2
   string    :placeOfBirth,  namespace: :rdaGr2
   string    :placeOfDeath,  namespace: :rdaGr2
+  string    :role,          namespace: :rdaGr2, namespace_field: :professionOrOccupation
   string    :gender,        namespace: :rdaGr2
   string    :isRelatedTo,   multi_value: true, namespace: :edm
   string    :hasMet,        multi_value: true, namespace: :edm
-  string    :sameAs,        multi_value: true, namespace: :sameAs
->>>>>>> Update concept serializer to use schema fields
+  string    :sameAs,        multi_value: true, namespace: :owl
 
   # Groups
   group :default do
@@ -72,29 +54,5 @@ class ConceptSchema < SupplejackApi::SupplejackSchema
   end
 
   build_object_id
-
-  # Groups
-  group :default do
-    fields [
-      :name,
-      :description,
-      :dateOfBirth,
-      :dateOfDeath,
-      :gender,
-    ]
-  end
-
-  group :all_fields do
-    includes [:internal_fields]
-    includes [:default]
-    fields [
-      :label,
-      :placeOfBirth,
-      :placeOfDeath,
-      :isRelatedTo,
-      :hasMet,
-      :sameAs,
-    ]
-  end
 
 end

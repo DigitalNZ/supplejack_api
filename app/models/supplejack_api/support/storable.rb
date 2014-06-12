d770e595962d4616e13b2ef2e09e970d6b2eefc8
Jesse <jessestegmann@gmail.com>
2014-06-12 17:03:40 +1200

    Remove indexes from storable

--- app/models/supplejack_api/support/storable.rb
+++ app/models/supplejack_api/support/storable.rb
@@ -1,31 +1,31 @@
-# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government, 
+# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government,
 # and is licensed under the GNU General Public License, version 3.
-# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and 
+# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and
 # the Department of Internal Affairs. http://digitalnz.org/supplejack
 
 module SupplejackApi
   module Support
     module Storable
       extend ActiveSupport::Concern
-  
-    	included do 
+
+    	included do
         include Mongoid::Document
     		include Mongoid::Timestamps
 
-        index status: 1
-        index internal_identifier: 1
-        index landing_url: 1
-        index updated_at: 1
-        
+        # index status: 1
+        # index internal_identifier: 1
+        # index landing_url: 1
+        # index updated_at: 1
+
         field :internal_identifier,         type: String
         field :landing_url,                 type: String
         field :status,                      type: String
 
         validates :internal_identifier,     presence: true
         validates :landing_url,             url: true
-      end  
+      end
     end
   end
 end

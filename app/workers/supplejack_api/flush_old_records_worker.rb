6eb715a980d326bd3eea9801da40eba2bb1e2120
Lukas Nguyen <lukas.nguyen9288@gmail.com>
2015-03-13 10:20:34 +1300

    Namespacing Record

--- app/workers/supplejack_api/flush_old_records_worker.rb
+++ app/workers/supplejack_api/flush_old_records_worker.rb
@@ -11,7 +11,7 @@
     @queue = :flush_records
 
     def self.perform(source_id, job_id)
-      Record.flush_old_records(source_id, job_id)
+      SupplejackApi::Record.flush_old_records(source_id, job_id)
     end
   end
 end

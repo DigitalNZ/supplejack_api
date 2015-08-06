ae9ecad3e1eb641d45d2804217a126a143d781e8
Lukas Nguyen <lukas.nguyen9288@gmail.com>
2015-08-06 16:49:13 +1200

    Update generator to use sunspot_rails 2.2

--- lib/generators/supplejack_api/install_generator.rb
+++ lib/generators/supplejack_api/install_generator.rb
@@ -74,7 +74,7 @@ module SupplejackApi
 
         inject_into_file('Gemfile', after: /^gem.*supplejack_api.*/) do
           string = [""]
-          string << "gem 'sunspot_rails', '~> 2.1.0'"
+          string << "gem 'sunspot_rails', '~> 2.2.0'"
           string << "gem 'active_model_serializers', git: 'https://github.com/boost/active_model_serializers.git'"
           string << "gem 'mongoid_auto_inc', git: 'https://github.com/boost/mongoid_auto_inc.git'"
 

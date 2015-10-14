2c48f83cf530a775c1ac974347c18c5d17af29db
Taylor Lodge <taylor@boost.co.nz>
2015-10-14 03:24:18 +0000

    Count items added to sets on creation

--- app/controllers/supplejack_api/concerns/user_sets_controller_metrics.rb
+++ app/controllers/supplejack_api/concerns/user_sets_controller_metrics.rb
@@ -5,6 +5,7 @@ module SupplejackApi
 
       included do
         after_action :create_set_record_view, only: :show
+        after_action :create_set_interaction, only: :create
 
         def create_set_record_view
           return unless @user_set
@@ -13,6 +14,13 @@ module SupplejackApi
             @user_set, 
             params[:request_logger_field]
           ) if params[:request_logger]
+        end
+
+        def create_set_interaction
+          return unless @user_set
+
+          record = SupplejackApi::Record.custom_find(@user_set.set_items.first.record_id)
+          SupplejackApi::InteractionModels::Set.create(interaction_type: :creation, facet: record.display_collection)
         end
       end
     end

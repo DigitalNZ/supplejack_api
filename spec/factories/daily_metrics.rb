a7cc23b64b7b04f5bbb5f900152b80e478095570
Taylor Lodge <taylor@boost.co.nz>
2016-01-26 13:32:18 +1300

    --wip--

--- spec/factories/daily_metrics.rb
+++ spec/factories/daily_metrics.rb
@@ -3,9 +3,8 @@ require 'faker'
 module SupplejackApi
   FactoryGirl.define do
     factory :daily_metrics, class: SupplejackApi::DailyMetrics do
-      total_active_records 10
-      total_new_records    1
-      date                 {Date.current}
+      total_public_sets 10
+      date              {Date.current}
     end
   end
 end
---
+++ spec/services/metrics_api/v3/endpoints/global_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module MetricsApi
+  module V3
+    module Endpoints
+      describe Global, slow: true do
+        let(:global) {Global.new({})}
+
+        describe "#call" do
+          it "returns an array of hashes representing the DailyMetrics for the supplied date range" do
+            create(:daily_metrics, date: Date.today, total_public_sets: 10)
+
+            expect(global.call).to eq([{
+              date: Date.today,
+              total_public_sets: 10
+            }])
+          end
+        end
+      end
+    end
+  end
+end

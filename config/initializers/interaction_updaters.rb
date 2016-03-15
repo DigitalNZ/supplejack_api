ee7342887b5059da3e2fedba7d95c4f05a84da37
Taylor Lodge <taylor@boost.co.nz>
2016-03-15 03:22:31 +0000

    Register AllUsageMetric updater and fix no delete_all method issue

--- config/initializers/interaction_updaters.rb
+++ config/initializers/interaction_updaters.rb
@@ -1,3 +1,7 @@
-[SupplejackApi::InteractionUpdaters::SetMetrics, SupplejackApi::InteractionUpdaters::UsageMetrics].each do |x|
+[
+  SupplejackApi::InteractionUpdaters::SetMetrics,
+  SupplejackApi::InteractionUpdaters::UsageMetrics,
+  SupplejackApi::InteractionUpdaters::AllUsageMetric
+].each do |x|
   SupplejackApi::InteractionMetricsWorker.register_interaction_updater(x)
 end

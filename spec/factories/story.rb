caf77189cff1d79b38796a07a486b1ff019596aa
Taylor Lodge <taylor@boost.co.nz>
2016-09-28 11:14:43 +1300

    Factory for creating UserSet Stories

--- spec/factories/story.rb
+++ spec/factories/story.rb
@@ -1,4 +1,27 @@
 FactoryGirl.define do
+  factory :story, class: SupplejackApi::UserSet do
+    transient do
+      number_of_story_items 2
+    end
+
+    name 'Story name'
+    description 'Story description'
+    user
+    featured false
+    approved false
+    tags ['story', 'tags']
+
+    after(:create) do |story, evaluator|
+      next unless story.set_items.empty?
+
+      evaluator.number_of_story_items.times do
+        story.set_items.build(attributes_for(:story_item))
+      end
+
+      story.save!
+    end
+  end
+
   factory :story_json, class: Hash do
     transient do
       number_of_blocks 2

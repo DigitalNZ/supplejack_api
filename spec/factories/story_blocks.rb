14f16ca9a6e6f8a8bab6e525f6efe3356323f6c2
Taylor Lodge <taylor@boost.co.nz>
2016-09-27 17:05:50 +1300

    DNZ Embed schema

--- spec/factories/story_blocks.rb
+++ spec/factories/story_blocks.rb
@@ -18,5 +18,34 @@ FactoryGirl.define do
       sub_type 'rich_text'
       content {{value: 'foo'}}
     end
+
+    factory :embed_dnz_block do
+      transient do
+        id 123
+        title 'A title'
+        display_collection 'Display collection'
+        category 'Category'
+        image_url 'http://foo.bar'
+        tags ['tags', 'yo']
+
+        alignment 'left'
+        caption 'a caption'
+      end
+
+      type 'embed'
+      sub_type 'dnz'
+      content {{
+        id: id,
+        title: title,
+        display_collection: display_collection,
+        category: category,
+        image_url: image_url,
+        tags: tags
+      }}
+      meta {{
+        alignment: alignment,
+        caption: caption
+      }}
+    end
   end
 end

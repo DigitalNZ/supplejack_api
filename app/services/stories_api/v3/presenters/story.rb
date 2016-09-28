bd363b33ad2b088d887078d73d02a706c930a167
Taylor Lodge <taylor@boost.co.nz>
2016-09-28 13:18:51 +1300

    Story presenter

---
+++ app/services/stories_api/v3/presenters/story.rb
@@ -0,0 +1,29 @@
+module StoriesApi
+  module V3
+    module Presenters
+      class Story
+        TOP_LEVEL_FIELDS = [
+          :id,
+          :name,
+          :description,
+          :privacy,
+          :featured,
+          :approved,
+          :tags
+        ]
+
+        def call(story)
+          result = {}
+
+          TOP_LEVEL_FIELDS.each do |field|
+            result[field] = story.send(field)
+          end
+          result[:number_of_items] = story.set_items.count
+          result[:contents] = story.set_items.map(&StoryItem)
+
+          result
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/stories_api/v3/presenters/story_spec.rb
@@ -0,0 +1,28 @@
+module StoriesApi
+  module V3
+    module Presenters
+      RSpec.describe Story do
+        let(:story) {create(:story)}
+        let(:presented_json) {subject.call(story)}
+
+        it 'presents all top level fields' do
+          [:id, :name, :description, :privacy, :featured, :approved, :tags].each do |field|
+            expect(presented_json[field]).to eq(story.send(field))
+          end
+        end
+
+        it 'presents number_of_items as the count of the items in the UserSet' do
+          expect(presented_json[:number_of_items]).to eq(story.set_items.count)
+        end
+
+        it 'presents the contents field as an array of valid StoryItems' do
+          expect(
+            presented_json[:contents].all? do |story_item|
+              ::StoriesApi::V3::Schemas::StoryItem::BlockValidator.new.call(story_item)
+            end
+          ).to eq(true)
+        end
+      end
+    end
+  end
+end

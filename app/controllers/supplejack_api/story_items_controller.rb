c8eb264406fdee043aa8e48da47d56ef29e1482f
Edwin Rozario <edwin@boost.co.nz>
2016-10-05 17:13:11 +1300

    Updating SetItemsController

--- app/controllers/supplejack_api/story_items_controller.rb
+++ app/controllers/supplejack_api/story_items_controller.rb
@@ -2,27 +2,32 @@
 module SupplejackApi
   class StoryItemsController < ApplicationController
     def index
-      render_response(:stories)
+      render_response(:story_items)
     end
 
     def show
-      render_response(:story)
+      render_response(:story_item)
     end
 
     def create
-      render_response(:stories)
+      render_response(:story_items)
     end
 
     def update
-      render_response(:story)
+      render_response(:story_item)
     end
 
     def destroy
-      render_response(:story)
+      render_response(:story_item)
     end
 
     private
 
+    # Renders the response for the controller action
+    #
+    # @author Eddie
+    # @last_modified Taylor
+    # @param endpoint [Symbol] the service endpoint
     def render_response(endpoint)
       api = StoriesApi::V3::Api.new(params.dup, endpoint, request.method.downcase.to_sym)
 
@@ -34,6 +39,11 @@ module SupplejackApi
       render json: api_response[:payload].to_json(include_root: false), status: api_response[:status] unless performed?
     end
 
+    # Renders error response for the controller action
+    #
+    # @author Eddie
+    # @last_modified Taylor
+    # @param endpoint [Symbol] the response
     def handle_errors(api_response)
       return unless api_response.key? :exception
 

51c109f1b0f9a16eb1f899af3570fa5cf44df8bb
Taylor Lodge <taylor@boost.co.nz>
2016-10-17 10:12:26 +1300

    Improve story item factory to have all block types

--- spec/factories/story_item.rb
+++ spec/factories/story_item.rb
@@ -1,16 +1,55 @@
 FactoryGirl.define do
   factory :story_item, class: SupplejackApi::SetItem do
     user_set
-    sequence(:record_id)
     sequence(:position)
-
+    sequence(:record_id)
     type 'text'
     sub_type 'heading'
-    content {{
-      value: 'foo'
-    }}
-    meta {{
-      size: 1
-    }}
+    content {{value: 'foo'}}
+    meta {{size: 1}}
+
+    factory :heading_item do
+      type 'text'
+      sub_type 'heading'
+      content {{value: 'foo'}}
+      meta {{size: 1}}
+    end
+
+    factory :rich_text_item do
+      type 'text'
+      sub_type 'rich_text'
+      content {{value: 'foo'}}
+    end
+
+    factory :embed_dnz_item do
+      transient do
+        id 123
+        title 'A title'
+        display_collection 'Display collection'
+        category 'Category'
+        image_url 'http://foo.bar'
+        tags ['tags', 'yo']
+
+        alignment 'left'
+        caption 'a caption'
+      end
+
+      type 'embed'
+      sub_type 'dnz'
+      content {{
+        record_id: id,
+        record: {
+          title: title,
+          display_collection: display_collection,
+          category: category,
+          image_url: image_url,
+          tags: tags
+        }
+      }}
+      meta {{
+        alignment: alignment,
+        caption: caption
+      }}
+    end
   end
 end

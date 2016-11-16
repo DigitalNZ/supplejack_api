e924adc21610cb20a39ff0809e8651c6398bbfe7
Taylor Lodge <taylor@boost.co.nz>
2016-11-17 10:49:30 +1300

    Fix Records without image_urls not passing schema validations

--- app/services/stories_api/v3/presenters/story.rb
+++ app/services/stories_api/v3/presenters/story.rb
@@ -11,7 +11,6 @@ module StoriesApi
     module Presenters
       class Story
         TOP_LEVEL_FIELDS = [
-          :id,
           :name,
           :description,
           :privacy,
--- app/services/stories_api/v3/schemas/story_item/embed/dnz.rb
+++ app/services/stories_api/v3/schemas/story_item/embed/dnz.rb
@@ -23,7 +23,7 @@ module StoriesApi
               optional(:title).filled(:str?)
               optional(:display_collection).filled(:str?)
               optional(:category).each(:str?)
-              optional(:image_url).filled(:str?)
+              optional(:image_url) { filled? > none? | int? }
               optional(:tags).each(:str?)
             end
 

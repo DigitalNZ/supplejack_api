26000c0ebe1f710b49113effd948fb92dd826488
Taylor Lodge <taylor@boost.co.nz>
2016-12-01 13:19:38 +1300

    Convert sidekiq session proxy to just push ids to be indexed/unindexed into redis where they get picked up by ClearIndexBuffer worker

--- lib/sunspot/sidekiq_session_proxy.rb
+++ lib/sunspot/sidekiq_session_proxy.rb
@@ -22,29 +22,10 @@ module Sunspot
         @buffer = SupplejackApi::IndexBuffer.new
         method = method.to_s.delete('!').to_sym
 
-        start = 0
         objects = objects.flatten.compact
         object_ids = objects.map { |o| o.id.to_s }.compact
-        total_ids_to_index = object_ids.size + @buffer.send("#{method}_record_ids_count")
 
-        if total_ids_to_index < 100
-          @buffer.send("#{method}_record_ids=", object_ids)
-        else
-          total_ids = object_ids + @buffer.pop_record_ids(method)
-          batch_object_ids = total_ids[start..(batch_size - 1)]
-          class_name = objects.first.class.name
-
-          while batch_object_ids do
-            index_worker_args = { class: class_name, id: batch_object_ids }
-
-            SupplejackApi::IndexWorker.perform_async(method, index_worker_args) if batch_object_ids.try(:any?)
-
-            start += batch_size
-            batch_object_ids = total_ids[start..(start + batch_size - 1)]
-          end
-
-          SupplejackApi::IndexWorker.perform_async(:commit)
-        end
+        @buffer.send("push_to_#{method}_buffer", object_ids)
       end
     end
 
@@ -56,10 +37,14 @@ module Sunspot
 
     def remove_all(clazz = nil)
       SupplejackApi::IndexWorker.perform_async(:remove_all, clazz.to_s)
+
+      commit
     end
 
     def remove_all!(clazz = nil)
       SupplejackApi::IndexWorker.perform_async(:remove_all, clazz.to_s)
+
+      commit
     end
 
     def commit(_soft_commit = false)

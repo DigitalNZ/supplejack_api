816b69f12b981fd66cc383a3f653bf85d4ea1260
Taylor Lodge <ubermouse894@gmail.com>
2016-12-05 10:17:39 +1300

    Formatting + FIXME comment update

--- app/services/stories_api/v3/presenters/content/embed/dnz.rb
+++ app/services/stories_api/v3/presenters/content/embed/dnz.rb
@@ -5,12 +5,19 @@ module StoriesApi
       module Content
         module Embed
           class Dnz
-            RECORD_FIELDS = { title: :title, display_collection: :display_collection,
-                              category: :category, image_url: :large_thumbnail_url,
-                              tags: :tag, description: :description }.freeze
+            RECORD_FIELDS = {
+              title: :title,
+              display_collection: :display_collection,
+              category: :category,
+              image_url: :large_thumbnail_url,
+              tags: :tag,
+              description: :description
+             }.freeze
 
             def call(block)
               # FIXME
+              # This is because I changed from record_id to id after Eddy migrated
+              # all the existing user sets
               record_id = block[:content][:id] || block[:content][:record_id]
               record = SupplejackApi::Record.find_by(record_id: record_id)
               result = { id: record_id.to_i }

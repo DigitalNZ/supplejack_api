ea31822f560433372a1f55d56c3241ecc2310300
Taylor Lodge <ubermouse894@gmail.com>
2016-12-05 10:16:07 +1300

    Updated FIXME comment with more description

--- app/services/stories_api/v3/endpoints/story_items.rb
+++ app/services/stories_api/v3/endpoints/story_items.rb
@@ -36,6 +36,7 @@ module StoriesApi
           item_params = params[:item].deep_symbolize_keys
 
           # FIXME: This is a temporary fix
+          # Can be removed after user_sets is retired
           item_params[:record_id] = item_params[:content][:id] if item_params[:content][:id]
 
           story_item = story.set_items.build(item_params)

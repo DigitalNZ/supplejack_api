b540b51282fcef765ab343c54ea1d4e683bdb71d
Reinkaos <rj.matthews23@gmail.com>
2017-05-22 15:54:44 +1200

    Add spec for the moderations controller

--- spec/controllers/supplejack_api/stories/moderations_controller_spec.rb
+++ spec/controllers/supplejack_api/stories/moderations_controller_spec.rb
@@ -6,13 +6,46 @@
 # Supplejack was created by DigitalNZ at the National Library of NZ and
 # the Department of Internal Affairs. http://digitalnz.org/supplejack
 
+require 'spec_helper'
+
 module SupplejackApi
   module Stories
-    RSpec.describe ModerationsController do
+    describe ModerationsController do
       routes { SupplejackApi::Engine.routes }
 
+      before(:each) do
+        @user = FactoryGirl.create(:user, authentication_token: "abc123")
+        allow(RecordSchema).to receive(:roles) { { admin: double(:admin, admin: true) } }
+        allow(controller).to receive(:authenticate_user!) { true }
+        allow(controller).to receive(:current_user) { @user }
+      end
+
       describe '#index' do
+        let!(:user_set) { FactoryGirl.create(:user_set, name: "Dogs and cats", priority: 5) }
+
+        before :each do
+          allow(controller).to receive(:authenticate_admin!) { true }
+          @normal_user = double(User, user_sets: []).as_null_object
+          allow(User).to receive(:find_by_api_key).with("nonadminkey") { @normal_user }
+        end
+
         it 'finds all public sets' do
+          expect(UserSet).to receive(:public_sets).with(page: nil) { [] }
+          get :index, format: 'json'
+        end
+
+        it 'renders the public sets as JSON' do
+          get :index, format: 'json'
+          sets = JSON.parse(response.body)
+
+          sets['sets'].each do |set|
+            expect(set).to have_key 'id'
+            expect(set).to have_key 'name'
+            expect(set).to have_key 'count'
+            expect(set).to have_key 'approved'
+            expect(set).to have_key 'created_at'
+            expect(set).to have_key 'updated_at'
+          end
         end
       end
     end

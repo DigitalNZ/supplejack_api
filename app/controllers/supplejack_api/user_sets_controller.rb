2ef448836a9d4c24d0fec42d3f119a7b9564fd20
Reinkaos <richard@boost.co.nz>
2017-08-31 15:07:23 +1200

    Update the user set serializers to work with the new version of AMS

--- app/controllers/supplejack_api/user_sets_controller.rb
+++ app/controllers/supplejack_api/user_sets_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government,
 # and is licensed under the GNU General Public License, version 3.
 # One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details.
@@ -17,7 +18,7 @@ module SupplejackApi
 
     def index
       @user_sets = current_user.user_sets
-      render json: serializable_array(@user_sets).to_json
+      render json: @user_sets, root: 'sets', adapter: :json
     end
 
     # Endpoint for the administrator to get a list of sets for any user.
@@ -27,7 +28,7 @@ module SupplejackApi
 
       if @user
         @user_sets = @user.user_sets
-        render json: serializable_array(@user_sets).to_json
+        render json: @user_sets, root: 'sets', user: true, adapter: :json
       else
         render json: { errors: "The user with api key: '#{params[:user_id]}' was not found" }, status: :not_found
       end
@@ -37,20 +38,20 @@ module SupplejackApi
     #
     def public_index
       @user_sets = UserSet.public_sets(page: params[:page])
-      render json: serializable_array(@user_sets, user: true, total: true).to_json
+      render json: @user_sets, root: 'sets', user: true, adapter: :json, meta: { total: UserSet.public_sets_count }
     end
 
     def featured_sets_index
       @user_sets = UserSet.featured_sets(4)
-      render json: serializable_array(@user_sets, user: true, featured: true).to_json
+      render json: @user_sets, root: 'sets', user: true, featured: true, adapter: :json
     end
 
     def show
       @user_set = UserSet.custom_find(params[:id])
       if @user_set
-        render json: UserSetSerializer.new(@user_set, user: current_user, fields: params[:fields])
+        render json: @user_set, user: true, root: 'set', adapter: :json
       else
-        render json: { errors: "Set with id: #{params[:id]} was not found." }, status: :not_found
+        rende json: { errors: "Set with id: #{params[:id]} was not found." }, status: :not_found
       end
     end
 
@@ -74,18 +75,6 @@ module SupplejackApi
     def destroy
       @user_set.destroy
       respond_with(@user_set)
-    end
-
-    private
-
-    def serializable_array(user_sets, options = {})
-      options.reverse_merge!(root: false, items: false, user: false, total: false)
-      hash = { 'sets' => [] }
-      user_sets.each do |set|
-        hash['sets'] << UserSetSerializer.new(set, options).as_json
-      end
-      hash['total'] = UserSet.public_sets_count if options[:total]
-      hash
     end
 
     rescue_from UserSet::WrongRecordsFormat do |_exception|
--- app/serializers/supplejack_api/concerns/search_serializable.rb
+++ app/serializers/supplejack_api/concerns/search_serializable.rb
@@ -2,23 +2,23 @@
 module SupplejackApi::Concerns::SearchSerializable
   extend ActiveSupport::Concern
 
-  included do
-    def serializable_hash
-      hash = {}
-      hash[:result_count] = object.total
-      hash[:results] = records_serialized_array
-      hash[:per_page] = object.per_page
-      hash[:page] = object.page
-      hash[:request_url] = object.request_url
-      hash[:solr_request_params] = object.solr_request_params if object.solr_request_params
-      hash[:warnings] = object.warnings if object.warnings.present?
-      hash[:suggestion] = object.collation if object.options[:suggest]
-      hash
-    end
-
-    def records_serialized_array
-      args = { fields: object.field_list, groups: object.group_list, scope: object.scope }
-      ActiveModel::ArraySerializer.new(object.results, args)
-    end
-  end
+  # included do
+  #   def serializable_hash
+  #     hash = {}
+  #     hash[:result_count] = object.total
+  #     hash[:results] = records_serialized_array
+  #     hash[:per_page] = object.per_page
+  #     hash[:page] = object.page
+  #     hash[:request_url] = object.request_url
+  #     hash[:solr_request_params] = object.solr_request_params if object.solr_request_params
+  #     hash[:warnings] = object.warnings if object.warnings.present?
+  #     hash[:suggestion] = object.collation if object.options[:suggest]
+  #     hash
+  #   end
+  #
+  #   def records_serialized_array
+  #     args = { fields: object.field_list, groups: object.group_list, scope: object.scope }
+  #     ActiveModel::ArraySerializer.new(object.results, args)
+  #   end
+  # end
 end

b912d7af5dd35a4cb2c1ba6b49a99d33f6cb01fe
Reinkaos <richard@boost.co.nz>
2017-10-05 10:18:47 +1300

    update specs and some rubocopping

--- app/services/determine_available_fields.rb
+++ app/services/determine_available_fields.rb
@@ -19,7 +19,7 @@ class DetermineAvailableFields
     # These fields are for paging between records
     fields += %i(next_page next_record previous_page previous_record)
 
-    fields += %i(updated_at created_at) unless options[:fields].present?
+    fields += %i(updated_at created_at) if options[:fields].blank?
 
     fields
   end

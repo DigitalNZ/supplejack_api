8d57f61edeb6b76542942365b3f8a306a30f0b68
Gustavo Motizuki <gustavo@boost.co.nz>
2017-10-27 16:08:19 +1300

    Adding user id to story moderation serializer

--- app/serializers/supplejack_api/stories_moderation_serializer.rb
+++ app/serializers/supplejack_api/stories_moderation_serializer.rb
@@ -8,6 +8,6 @@
 
 module SupplejackApi
   class StoriesModerationSerializer < ActiveModel::Serializer
-    attributes :id, :name, :count, :approved, :featured, :created_at, :updated_at, :featured_at
+    attributes :id, :name, :count, :approved, :featured, :created_at, :updated_at, :featured_at, :user_id
   end
 end

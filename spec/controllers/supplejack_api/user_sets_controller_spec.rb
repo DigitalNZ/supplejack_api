9f2959ceab2cf42f1f13745e898f8d6024b246ea
Oliver Stigley <oliver@boost.co.nz>
2017-11-22 11:59:03 +1300

    user sets controller specs passing

--- spec/controllers/supplejack_api/user_sets_controller_spec.rb
+++ spec/controllers/supplejack_api/user_sets_controller_spec.rb
@@ -39,23 +39,23 @@ module SupplejackApi
 
         it "authenticates the user as an admin" do
           expect(controller).to receive(:authenticate_admin!) { true }
-          get :admin_index, user_id: "nonadminkey"
+          get :admin_index, params: { user_id: "nonadminkey" }
         end
 
         it "finds the user from the :user_id" do
           expect(User).to receive(:find_by_api_key).with("nonadminkey")
-          get :admin_index, user_id: "nonadminkey"
+          get :admin_index, params: { user_id: "nonadminkey" }
         end
 
         it "assigns the user's sets to @user_sets" do
           expect(@normal_user).to receive(:user_sets) { [] }
-          get :admin_index, user_id: "nonadminkey"
+          get :admin_index, params: { user_id: "nonadminkey" }
           expect(assigns(:user_sets)).to eq []
         end
 
         it "renders a error when the user is not found" do
           allow(User).to receive(:find_by_api_key) { nil }
-          get :admin_index, user_id: "whatever"
+          get :admin_index, params: { user_id: "whatever" }
           expect(response.code).to eq "404"
           expect(response.body).to eq({errors: "The user with api key: 'whatever' was not found"}.to_json)
         end
@@ -64,7 +64,7 @@ module SupplejackApi
       context "authentication fails" do
         it "renders a error when the admin authentication fails" do
           allow(controller).to receive(:current_user) { double(:developer, admin?: false, role: 'developer') }
-          get :admin_index, user_id: "nonadminkey", format: "json"
+          get :admin_index, params: { user_id: "nonadminkey" }, format: "json"
           expect(response.code).to eq "403"
           expect(response.body).to eq({errors: "You need Administrator privileges to perform this request"}.to_json)
         end
@@ -108,12 +108,12 @@ module SupplejackApi
 
       it "finds the @user_set" do
         expect(UserSet).to receive(:custom_find).with(@user_set.id.to_s) { @user_set }
-        get :show, id: @user_set.id.to_s
+        get :show, params: { id: @user_set.id.to_s }
       end
 
       it "returns a 404 error when the set is not found" do
         allow(UserSet).to receive(:custom_find) { nil }
-        get :show, id: @user_set.id.to_s
+        get :show, params: { id: @user_set.id.to_s }
         expect(response.code).to eq("404")
         expect(response.body).to eq({errors: "Set with id: #{@user_set.id.to_s} was not found."}.to_json)
       end
@@ -130,26 +130,26 @@ module SupplejackApi
       it "should build a new set with the params" do
         expect(controller.current_user.user_sets).to receive(:build) { @user_set }
         expect(@user_set).to receive(:update_attributes_and_embedded).with({"name" => "Dogs", "description" => "Ugly", "privacy" => "hidden"})
-        post :create, set: {"name" => "Dogs", "description" => "Ugly", "privacy" => "hidden"}
+        post :create, params: { set: {"name" => "Dogs", "description" => "Ugly", "privacy" => "hidden"} }
       end
 
       it "saves the user set" do
         pending # This is broken because it doesn't create sets properly and I don't know how to make it create sets properly
         expect(@user_set).to receive(:save).and_return(true)
-        post :create, set: {}
+        post :create, params: { set: {} }
       end
 
       it "returns a 422 error when the set is invalid" do
         allow(@user_set).to receive(:update_attributes_and_embedded) { false }
         allow(@user_set).to receive(:errors).and_return({name: ["can't be blank"]})
-        post :create, set: {}
+        post :create, params: { set: {} }
         expect(response.code).to eq("422")
         expect(response.body).to eq({errors: {name: ["can't be blank"]}}.to_json)
       end
 
       it "rescues from a :records format error and renders the error" do
         allow(@user_set).to receive(:update_attributes_and_embedded).and_raise(UserSet::WrongRecordsFormat)
-        post :create, set: {}
+        post :create, params: { set: {} }
         expect(response.code).to eq("422")
         expect(response.body).to eq({errors: {records: ["The records array is not in a valid format."]}}.to_json)
       end
@@ -168,37 +168,38 @@ module SupplejackApi
 
         it "finds the @user_set through the user" do
           expect(controller.current_user.user_sets).to receive(:custom_find).with(@user_set.id.to_s) { @user_set }
-          put :update, id: @user_set.id.to_s, set: {records: [{record_id: 13, position: 2}]}
+          put :update, params: { id: @user_set.id.to_s, set: {records: [{record_id: 13, position: 2}]} }
         end
 
         it "returns a 404 error when the set is not found" do
           allow(controller.current_user.user_sets).to receive(:custom_find) { nil }
-          put :update, id: @user_set.id.to_s
+          put :update, params: { id: @user_set.id.to_s }
           expect(response.code).to eq("404")
           expect(response.body).to eq({errors: "Set with id: #{@user_set.id.to_s} was not found."}.to_json)
         end
 
         it "updates the attributes of the @user_set" do
           expect(@user_set).to receive(:update_attributes_and_embedded).with({"records" => [{"record_id" => "13", "position" => "2"}]}, @user)
-          put :update, id: @user_set.id.to_s, set: {records: [{record_id: 13, position: 2}]}
+          put :update, params: { id: @user_set.id.to_s, set: {records: [{record_id: 13, position: 2}]} }
         end
 
         it "updates the approved attribute of a @user_set" do
-          expect(@user_set).to receive(:update_attributes_and_embedded).with({"approved" => true}, @user)
-          put :update, id: @user_set.id.to_s, set: {approved: true}
+          # Rails 5 converts boolean params to string, which has no affect on mongo result
+          expect(@user_set).to receive(:update_attributes_and_embedded).with({"approved" => 'true'}, @user)
+          put :update, params: { id: @user_set.id.to_s, set: {approved: true} }
         end
 
         it "returns a 406 error when the set is invalid" do
           allow(@user_set).to receive(:update_attributes_and_embedded) { false }
           allow(@user_set).to receive(:errors).and_return({name: ["can't be blank"]})
-          post :update, id: @user_set.id.to_s, set: {name: nil}
+          post :update, params: { id: @user_set.id.to_s, set: {name: nil} }
           expect(response.code).to eq("422")
           expect(response.body).to eq({errors: {name: ["can't be blank"]}}.to_json)
         end
 
         it "rescues from a :records format error and renders the error" do
           allow(@user_set).to receive(:update_attributes_and_embedded).and_raise(UserSet::WrongRecordsFormat)
-          post :update, id: @user_set.id.to_s, set: {name: nil}
+          post :update, params: { id: @user_set.id.to_s, set: {name: nil} }
           expect(response.code).to eq("422")
           expect(response.body).to eq({errors: {records: ["The records array is not in a valid format."]}}.to_json)
         end
@@ -214,19 +215,19 @@ module SupplejackApi
 
       it "returns a 404 error when the set is not found" do
         allow(controller.current_user.user_sets).to receive(:custom_find) { nil }
-        delete :destroy, id: @user_set.id.to_s
+        delete :destroy, params: { id: @user_set.id.to_s }
         expect(response.code).to eq("404")
         expect(response.body).to eq({errors: "Set with id: #{@user_set.id.to_s} was not found."}.to_json)
       end
 
       it "finds the @user_set through the user" do
         expect(controller.current_user.user_sets).to receive(:custom_find).with(@user_set.id.to_s) { @user_set }
-        delete :destroy, id: @user_set.id.to_s, format: :json
+        delete :destroy, params: { id: @user_set.id.to_s }, format: :json
       end
 
       it "deletes the user set" do
         expect(@user_set).to receive(:destroy)
-        delete :destroy, id: @user_set.id.to_s, format: :json
+        delete :destroy, params: { id: @user_set.id.to_s }, format: :json
       end
     end
   end

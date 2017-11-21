5e69de6ce41fbb6a97ff4908c7d4419e40a5e654
Oliver Stigley <oliver@boost.co.nz>
2017-11-21 15:49:05 +1300

    sources spec passing

--- spec/factories/sources.rb
+++ spec/factories/sources.rb
@@ -8,6 +8,7 @@
 module SupplejackApi
   FactoryBot.define do
     factory :source, class: SupplejackApi::Source do
+      association :partner, factory: :partner
       name        'Sample source'
       source_id   '1234'
     end

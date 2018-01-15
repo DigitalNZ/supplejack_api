cd6b45dd44ae5f94ad522c62633f2864388a3200
Oliver Stigley <oliver@boost.co.nz>
2018-01-15 15:35:38 +1300

    change ujs in generator

--- lib/generators/supplejack_api/install_generator.rb
+++ lib/generators/supplejack_api/install_generator.rb
@@ -90,9 +90,9 @@ module SupplejackApi
 
       def add_assets
         puts "\nAdding assets "
-        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highcharts\n", :after => "jquery_ujs\n"
-        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highcharts-more\n", :after => "jquery_ujs\n"
-        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highstock\n", :after => "jquery_ujs\n"
+        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highcharts\n", :after => "rails-ujs\n"
+        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highcharts-more\n", :after => "rails-ujs\n"
+        insert_into_file "app/assets/javascripts/application.js", "//= require highcharts/highstock\n", :after => "rails-ujs\n"
         insert_into_file "app/assets/stylesheets/application.css", "\n *= require supplejack_api/application", :after => "require_self"
       end
 

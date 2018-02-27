c1218382b179ed9e990d508ba32d40adcd9c792f
Theo Paul <theo.markkus.paul@gmail.com>
2018-02-27 17:58:47 +1300

    Remove license from spec files

--- spec/dummy/config/initializers/devise.rb
+++ spec/dummy/config/initializers/devise.rb
@@ -1,9 +1,4 @@
-# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3.
-# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and 
-# the Department of Internal Affairs. http://digitalnz.org/supplejack
+
 
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.

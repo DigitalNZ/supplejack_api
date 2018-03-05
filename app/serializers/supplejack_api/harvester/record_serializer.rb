02728a1abd48ed6b4ef291419a33a688a86ab9c1
Oliver Stigley <oliver@boost.co.nz>
2018-03-05 13:26:02 +1300

    harvester records index uses search record serializer

--- app/serializers/supplejack_api/harvester/record_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SupplejackApi
-  module Harvester
-    class RecordSerializer < ActiveModel::Serializer
-      attributes :id, :fragments
-    end
-  end
-end

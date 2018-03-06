82f8a6a7547edf2f7565b3d13b654fa4f609672b
Oliver Stigley <oliver@boost.co.nz>
2018-03-07 09:56:42 +1300

    fixed preview records controller specs

---
+++ spec/factories/preview_records.rb
@@ -0,0 +1,10 @@
+module SupplejackApi
+  FactoryBot.define do
+    factory :preview_record, class: SupplejackApi.config.preview_record_class do
+      record_type 0
+      internal_identifier 'youtube:fngqeb8ane8'
+      status 'active'
+      record_id 54
+    end
+  end
+end

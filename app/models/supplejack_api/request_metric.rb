5d1a1fc154d70d0d96c60de355341a05d70a6d3c
Reinkaos <richard@boost.co.nz>
2018-03-21 15:27:41 +1300

    Optimize the request metric summarization

--- app/models/supplejack_api/request_metric.rb
+++ app/models/supplejack_api/request_metric.rb
@@ -21,13 +21,9 @@ module SupplejackApi
     def self.summarize
       return unless SupplejackApi.config.log_metrics == true
 
-      # Split the requests by date
       summarized_metrics = all.group_by(&:date).each_with_object({}) do |(date, metrics), summary|
-        uniq_records = metrics.flat_map(&:records).uniq
-        summary[date] = {}
-
-        uniq_records.each do |record|
-          summary[date][record['record_id']] = {
+        summary[date] = Hash.new do |hash, key|
+          hash[key] = {
             'metrics' => {
               'page_views' => 0,
               'user_set_views' => 0,
@@ -41,21 +37,14 @@ module SupplejackApi
           }
         end
 
-        # Group them by metric
-        metrics.group_by(&:metric).each do |metric, results|
-          metric_total_records = results.flat_map(&:records)
-          metric_uniq_records = metric_total_records.uniq
-
-          # Total the number of records within a date/metric
-          metric_uniq_records.map do |record|
-            record_id = record['record_id']
-            summary[date][record_id]['metrics'][metric] = metric_total_records.count(record)
-            summary[date][record_id]['display_collection'] = record['display_collection']
+        metrics.each do |metric|
+          metric.records.each do |record|
+            summary[date][record['record_id']]['metrics'][metric.metric] += 1
+            summary[date][record['record_id']]['display_collection'] = record['display_collection']
           end
         end
       end
 
-      # Generate Record Metrics
       summarized_metrics.each do |date, record|
         record.each do |id, details|
           RecordMetric.spawn(id, details['metrics'], details['display_collection'], date)

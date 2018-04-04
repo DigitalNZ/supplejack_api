ac22a8907493f3f237b8ccf2f45aa0837439478b
Reinkaos <richard@boost.co.nz>
2018-04-05 08:57:01 +1200

    Remove validation specs for story item content as the field is no longer required

--- spec/services/stories_api/v3/schemas/story_item/text/rich_text_spec.rb
+++ spec/services/stories_api/v3/schemas/story_item/text/rich_text_spec.rb
@@ -7,15 +7,6 @@ module StoriesApi
             let(:valid_block) { build(:rich_text_block) }
 
             describe '#content' do
-              context 'invalid' do
-                it 'requires a value' do
-                  result = subject.call(valid_block.update(content: {}))
-
-                  expect(result.success?).to eq(false)
-                  expect(result.messages).to include(:content)
-                end
-              end
-
               context 'valid' do
                 it 'is valid with value present' do
                   result = subject.call(valid_block)

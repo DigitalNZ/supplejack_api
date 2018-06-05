259ab1a0564c72f27c8ead84aa4916f5d7378649
Reinkaos <richard@boost.co.nz>
2018-06-05 13:07:10 +1200

    Add the flush old records worker and index source worker to the engine

---
+++ app/workers/supplejack_api/index_source_worker.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and
+# the Department of Internal Affairs. http://digitalnz.org/supplejack
+
+module SupplejackApi
+  class IndexSourceWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: 'default'
+
+    def perform(source_id, date=nil)
+      cursor = if date.present?
+                 SupplejackApi.config.record_class.where(:'fragments.source_id' => source_id, :updated_at.gt => Time.parse(date))
+               else
+                 SupplejackApi.config.record_class.where('fragments.source_id': source_id)
+               end
+
+      in_chunks(cursor.where(status: "active")) do |records|
+        Sunspot.index(records)
+      end
+
+      in_chunks(cursor.where(status: "deleted")) do |records|
+        Sunspot.remove(records)
+      end
+    end
+
+    def in_chunks(cursor, &block)
+      total = cursor.count
+      start = 0
+      chunk_size = 10000
+      while start < total
+        records = cursor.limit(chunk_size).skip(start)
+        yield records
+        start += chunk_size
+      end
+    end
+  end
+end

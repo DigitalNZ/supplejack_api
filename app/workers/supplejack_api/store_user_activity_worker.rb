7ab6b517aa7ff91d1132869cad31c1f6a7ea9f50
Gustavo Motizuki <gustavo@boost.co.nz>
2018-06-11 13:13:47 +1200

    Move store user activity worker and add tests to it

---
+++ app/workers/supplejack_api/store_user_activity_worker.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module SupplejackApi
+  class StoreUserActivityWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: 'critical'
+
+    def perform
+      Rails.logger.info '[StoreUserActivityWorker] Starting Execution'
+
+      all_users = User.all.no_timeout
+      Rails.logger.error '[StoreUserActivityWorker] No users were returned from mongo' if all_users.empty?
+
+      all_users.each do |user|
+        begin
+          if user.daily_activity_stored
+            Rails.logger.warn "[StoreUserActivityWorker] Skipping Reset - Daily activity already stored for #{user}"
+            next
+          end
+          user.user_activities << UserActivity.build_from_user(user.daily_activity)
+          user.calculate_last_30_days_requests
+          user.reset_daily_activity
+          user.save!
+        rescue StandardError => e
+          Rails.logger.error "[StoreUserActivityWorker] Failed resetting activity for #{user} with exception #{e}"
+          next
+        end
+      end
+
+      Rails.logger.info '[StoreUserActivityWorker] Generating SiteActivity'
+      SiteActivity.generate_activity
+      Rails.logger.info '[StoreUserActivityWorker] Completed SiteActivity'
+    rescue StandardError => e
+      Rails.logger.error "[StoreUserActivityWorker] Exception (#{e})"
+    end
+  end
+end
---
+++ spec/workers/supplejack_api/store_user_activity_worker_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+module SupplejackApi
+  describe StoreUserActivityWorker do
+    let!(:users) { create_list(:user, 10, daily_activity_stored: false) }
+    before do
+      users.each do |user|
+        user.daily_activity = {
+          records: {
+            show: 2449
+          },
+          stories: {
+            show: 522,
+            admin_index: 157,
+            update: 48
+          },
+          search: {
+            records: 1185
+          },
+          concepts: {
+            show: 3
+          },
+          user_sets: {
+            create_item: 15,
+            destroy_item: 9,
+            update_item: 9
+          }
+        }
+        user.save!
+      end
+    end
+
+    it 'successfuly reset daily activity' do
+      users.each do |user|
+        expect(user.daily_activity).not_to be_nil
+      end
+      subject.perform
+      users.each do |user|
+        user.reload
+        expect(user.daily_activity).to be_nil
+        expect(user.daily_activity_stored).to be_truthy
+      end
+    end
+
+    it 'logs an error when failed to reset daily activities' do
+      allow_any_instance_of(SupplejackApi::User).to receive(:save!).and_raise(StandardError)
+      expect(Rails.logger).to receive(:error).exactly(10).times
+      subject.perform
+    end
+
+    it 'logs a warn when reset daily activity job is called on user more than once a day' do
+      user = users.first
+      user.daily_activity_stored = true
+      user.save!
+      expect(Rails.logger).to receive(:warn).exactly(1).times
+      subject.perform
+    end
+
+    it 'logs as error when generate_activity raise an exception' do
+      allow(SupplejackApi::SiteActivity).to receive(:generate_activity).and_raise(StandardError)
+      expect(Rails.logger).to receive(:error).exactly(1).times
+      subject.perform
+    end
+  end
+end

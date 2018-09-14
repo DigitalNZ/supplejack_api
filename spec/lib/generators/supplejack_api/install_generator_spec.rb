e2eadb61ccc4587af433f429d1420f0ae585380c
Oliver Stigley <oliver@boost.co.nz>
2018-09-14 15:32:14 +1200

    specs for generator config files

--- spec/lib/generators/supplejack_api/install_generator_spec.rb
+++ spec/lib/generators/supplejack_api/install_generator_spec.rb
@@ -4,7 +4,9 @@ require 'generator_spec'
 module SupplejackApi
   module Generators
     describe InstallGenerator, type: :generator do
-      destination File.expand_path("../tmp/", __FILE__)
+      destination_path = File.expand_path("../tmp/", __FILE__)
+      generator_files_path = File.expand_path(Rails.root)
+      destination(destination_path)
 
       before(:all) do
         prepare_destination
@@ -12,10 +14,26 @@ module SupplejackApi
       end
 
       describe '#config files' do
-        let(:generated_application_yml) { File.read(destination)}
-        it 'adds details to application yml' do
-          binding.pry
-          expect(true).to be true
+        let(:generated_application_yml) { File.read("#{destination_path}/config/application.yml") }
+        let(:generated_schedule) { File.read("#{destination_path}/config/schedule.rb") }
+        let(:example_schedule) { File.read("#{generator_files_path}/config/schedule.example.rb")}
+
+        it 'adds details to application.yml' do
+          File.open("#{generator_files_path}/config/application.yml.example").each do |line|
+            expect(generated_application_yml).to include line
+          end
+        end
+
+        it 'adds details to schedule.rb' do
+          expect(generated_schedule).to include example_schedule
+        end
+
+        it 'creates mongoid.yml' do
+          assert_file("#{destination_path}/config/mongoid.yml")
+        end
+
+        it 'creates sunspot.yml' do
+          assert_file("#{destination_path}/config/sunspot.yml")
         end
       end
     end

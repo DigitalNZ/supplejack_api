796756135b456ba6ba15b5122021d9a79c6acd2d
Gustavo Motizuki <gustavo@boost.co.nz>
2018-09-19 16:19:03 +1200

    set correct date for metrics

--- app/models/supplejack_api/request_metric.rb
+++ app/models/supplejack_api/request_metric.rb
@@ -5,14 +5,14 @@ module SupplejackApi
   class RequestMetric
     include Mongoid::Document
 
-    field :date,    type: Date,   default: Time.current.utc
+    field :date,    type: Date,   default: Time.zone.now.beginning_of_day
     field :records, type: Array,  default: []
     field :metric,  type: String
 
     validates :records, presence: true
     validates :metric,  presence: true
 
-    def self.spawn(records, metric, date = Time.current.utc)
+    def self.spawn(records, metric, date = Time.zone.now.beginning_of_day)
       return unless SupplejackApi.config.log_metrics == true
       create(records: records, metric: metric, date: date)
     end

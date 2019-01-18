e99fa4eb124ce6af62d557ac90be24c1f9c8c10d
Theo Paul <theo@boost.co.nz>
2019-01-18 17:01:05 +1300

    Add processed_by_top_collection_metrics field to the RecordMetric model

--- app/models/supplejack_api/record_metric.rb
+++ app/models/supplejack_api/record_metric.rb
@@ -5,18 +5,19 @@ module SupplejackApi
   class RecordMetric
     include Mongoid::Document
 
-    field :date,                            type: Date,    default: Time.zone.now.beginning_of_day
-    field :record_id,                       type: Integer
-    field :page_views,                      type: Integer, default: 0
-    field :user_set_views,                  type: Integer, default: 0
-    field :display_collection,              type: String
-    field :user_story_views,                type: Integer, default: 0
-    field :added_to_user_sets,              type: Integer, default: 0
-    field :source_clickthroughs,            type: Integer, default: 0
-    field :appeared_in_searches,            type: Integer, default: 0
-    field :added_to_user_stories,           type: Integer, default: 0
-    field :processed_by_collection_metrics, type: Boolean, default: false
-    field :processed_by_top_metrics,        type: Boolean, default: false
+    field :date,                                type: Date,    default: Time.zone.now.beginning_of_day
+    field :record_id,                           type: Integer
+    field :page_views,                          type: Integer, default: 0
+    field :user_set_views,                      type: Integer, default: 0
+    field :display_collection,                  type: String
+    field :user_story_views,                    type: Integer, default: 0
+    field :added_to_user_sets,                  type: Integer, default: 0
+    field :source_clickthroughs,                type: Integer, default: 0
+    field :appeared_in_searches,                type: Integer, default: 0
+    field :added_to_user_stories,               type: Integer, default: 0
+    field :processed_by_collection_metrics,     type: Boolean, default: false
+    field :processed_by_top_metrics,            type: Boolean, default: false
+    field :processed_by_top_collection_metrics, type: Boolean, default: false
 
     validates :record_id, presence: true
     validates :record_id, uniqueness: { scope: :date }

1bf482f2f47d2dbfb083910ef4da69493c522c02
Tim Wright <tim@boost.co.nz>
2019-02-26 15:36:06 +1300

    compact api response

--- app/services/metrics_api/v3/presenters/top_records.rb
+++ app/services/metrics_api/v3/presenters/top_records.rb
@@ -12,12 +12,12 @@ module MetricsApi
 
         # rubocop:disable Metrics/LineLength
         def to_json
-          page_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'page_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'page_views', display_collection: @m.display_collection) : {}
-          user_set_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection) : {}
-          user_story_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection) : {}
-          source_clickthroughs = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection) : {}
-          appeared_in_searches = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection) : {}
-          added_to_user_stories = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection) : {}
+          page_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'page_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'page_views', display_collection: @m.display_collection) : nil
+          user_set_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection) : nil
+          user_story_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection) : nil
+          source_clickthroughs = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection) : nil
+          appeared_in_searches = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection) : nil
+          added_to_user_stories = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection) : nil
 
           {
             id: @m.display_collection,
@@ -27,7 +27,7 @@ module MetricsApi
             source_clickthroughs: source_clickthroughs,
             appeared_in_searches: appeared_in_searches,
             added_to_user_stories: added_to_user_stories
-          }
+          }.compact
         end
         # rubocop:enable Metrics/LineLength
 

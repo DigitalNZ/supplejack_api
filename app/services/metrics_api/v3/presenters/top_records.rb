f8bb0964009efe18dbba8eabe7fc8196dba5b747
Tim Wright <tim@boost.co.nz>
2019-02-26 16:30:03 +1300

    tidy up

--- app/services/metrics_api/v3/presenters/top_records.rb
+++ app/services/metrics_api/v3/presenters/top_records.rb
@@ -6,28 +6,13 @@ module MetricsApi
       # Presents a +SupplejackApi::TopCollectionMetric+ ready to be returned via the API
       class TopRecords
         def initialize(metric)
+          binding.pry
           @m = metric
         end
 
-        # rubocop:disable Metrics/LineLength
         def to_json
-          page_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'page_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'page_views', display_collection: @m.display_collection).r : nil
-          user_set_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_set_views', display_collection: @m.display_collection).r : nil
-          user_story_views = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'user_story_views', display_collection: @m.display_collection).r : nil
-          source_clickthroughs = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'source_clickthroughs', display_collection: @m.display_collection).r : nil
-          appeared_in_searches = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'appeared_in_searches', display_collection: @m.display_collection).r : nil
-          added_to_user_stories = SupplejackApi::TopCollectionMetric.where(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection).exists? ? SupplejackApi::TopCollectionMetric.find_by(date: @m.date, metric: 'added_to_user_stories', display_collection: @m.display_collection).r : nil
-
-          {
-            page_views: page_views,
-            user_set_views: user_set_views,
-            user_story_views: user_story_views,
-            source_clickthroughs: source_clickthroughs,
-            appeared_in_searches: appeared_in_searches,
-            added_to_user_stories: added_to_user_stories
-          }.compact
+          { @m.metric: @m.results }
         end
-        # rubocop:enable Metrics/LineLength
 
         def self.to_proc
           ->(metric) { new(metric).to_json }

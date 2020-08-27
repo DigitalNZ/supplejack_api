27f37ccb0bfc992030c1df8532cb6e6c73fc6b3a
Richard Matthews <richard@boost.co.nz>
2020-08-27 15:59:48 +1200

    5000 ms timeout

--- app/models/supplejack_api/search.rb
+++ app/models/supplejack_api/search.rb
@@ -240,7 +240,7 @@ module SupplejackApi
           params['q.op'] = 'AND'
           params['df'] = 'text'
           params['sow'] = 'true'
-          params['timeAllowed'] = '10000'
+          params['timeAllowed'] = '5000'
         end
 
         # Facet Queries

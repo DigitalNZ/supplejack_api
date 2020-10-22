164e14281aeb8e5baf3ee33b0f1a3a68341ccd19
Richard Matthews <richard@boost.co.nz>
2020-10-23 08:39:02 +1300

    Add module and default batch size

--- lib/supplejack_api/index_processor.rb
+++ lib/supplejack_api/index_processor.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
-class IndexProcessor
-  attr_accessor :batch_size
+module SupplejackApi
+  class IndexProcessor
+    attr_accessor :batch_size
 
-  def initialize(batch_size)
-    @batch_size = batch_size
-  end
+    def initialize(batch_size)
+      @batch_size = batch_size || 1000
+    end
 
-  def call
-    p 'Looking for records to index..'
+    def call
+      p 'Looking for records to index..'
 
-    records_to_index = SupplejackApi::Record.ready_for_processing.where(status: 'active').limit(batch_size)
+      records_to_index = SupplejackApi::Record.ready_for_processing.where(status: 'active').limit(batch_size)
 
-    p "There are #{records_to_index.to_a.count} records to be indexed.."
+      p "There are #{records_to_index.to_a.count} records to be indexed.."
 
-    BatchIndexRecords.new(records_to_index).call
+      BatchIndexRecords.new(records_to_index).call
 
-    p 'Looking for records to remove..'
+      p 'Looking for records to remove..'
 
-    records_to_remove = SupplejackApi::Record.ready_for_processing.where(:status.ne => 'active').limit(batch_size)
+      records_to_remove = SupplejackApi::Record.ready_for_processing.where(:status.ne => 'active').limit(batch_size)
 
-    p "There are #{records_to_remove.to_a.count} records to be removed from the index.."
+      p "There are #{records_to_remove.to_a.count} records to be removed from the index.."
 
-    BatchRemoveRecordsFromIndex.new(records_to_remove).call
+      BatchRemoveRecordsFromIndex.new(records_to_remove).call
+    end
   end
 end

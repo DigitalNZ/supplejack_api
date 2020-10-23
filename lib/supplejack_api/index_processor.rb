77106b99e55c0ba606212685a52420ddd5cc160a
Richard Matthews <richard@boost.co.nz>
2020-10-23 14:15:39 +1300

    remove break and loop

--- lib/supplejack_api/index_processor.rb
+++ lib/supplejack_api/index_processor.rb
@@ -9,38 +9,24 @@ module SupplejackApi
     end
 
     def call
-      loop do
-        p 'Looking for records to index..' unless Rails.env.test?
+      p 'Looking for records to index..' unless Rails.env.test?
 
-        records_to_index = SupplejackApi::Record.ready_for_indexing.where(status: 'active')
+      records_to_index = SupplejackApi::Record.ready_for_indexing.where(status: 'active')
 
-        records_to_index.in_groups_of(size).each do |records|
-          p "There are #{records_to_index.to_a.count} records to be indexed.." unless Rails.env.test?
+      records_to_index.in_groups_of(size).each do |records|
+        p "There are #{records_to_index.to_a.count} records to be indexed.." unless Rails.env.test?
 
-          BatchIndexRecords.new(records.compact).call
-        end
+        BatchIndexRecords.new(records.compact).call
+      end
 
-        p 'Looking for records to remove..'
+      p 'Looking for records to remove..'
 
-        records_to_remove = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active')
+      records_to_remove = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active')
 
-        records_to_remove.in_groups_of(size).each do |records|
-          p "There are #{records_to_remove.to_a.count} records to be removed from the index.." unless Rails.env.test?
+      records_to_remove.in_groups_of(size).each do |records|
+        p "There are #{records_to_remove.to_a.count} records to be removed from the index.." unless Rails.env.test?
 
-          BatchRemoveRecordsFromIndex.new(records.compact).call
-        end
-
-        p 'Checking if remaining records are above the threshold...' unless Rails.env.test?
-
-        records_to_index = SupplejackApi::Record.ready_for_indexing.where(status: 'active')
-        records_to_remove = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active')
-
-        if records_to_index.count < 100 && records_to_remove.count < 100
-          p 'There are not enough records at the moment. Will try again soon...' unless Rails.env.test?
-          break
-        else
-          p 'Continuing to index records...' unless Rails.env.test?
-        end
+        BatchRemoveRecordsFromIndex.new(records.compact).call
       end
     end
   end

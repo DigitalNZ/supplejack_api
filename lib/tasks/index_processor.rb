4c8671606f4b30bf9289d051a0d7688a86168b86
Richard Matthews <richard@boost.co.nz>
2020-11-03 14:04:54 +1300

    Fix index processor to work if you do not pass it the size

--- lib/tasks/index_processor.rb
+++ lib/tasks/index_processor.rb
@@ -1,7 +1,13 @@
+require 'rake'
+
 namespace :index_processor do
   task :run, [:batch_size] => [:environment] do |_, args|
     loop do
-      SupplejackApi::IndexProcessor.new(args[:batch_size].to_i).call
+      if args[:batch_size]
+        SupplejackApi::IndexProcessor.new(args[:batch_size].to_i).call
+      else
+        SupplejackApi::IndexProcessor.new.call
+      end
       sleep 30
     end
   end

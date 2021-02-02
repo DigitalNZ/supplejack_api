a90fcf78d82e544636a9e06d685509ed04ed904b
Edwin Rozario <edwin@boost.co.nz>
2021-02-02 16:06:39 +1300

    test(SupplejackApi::StoriesModerationSerializer): update

--- spec/serializers/supplejack_api/stories_moderation_serializer_spec.rb
+++ spec/serializers/supplejack_api/stories_moderation_serializer_spec.rb
@@ -4,36 +4,44 @@ require 'spec_helper'
 
 module SupplejackApi
   describe StoriesModerationSerializer do
-    let(:user_set) { FactoryBot.create(:user_set, name: "Dogs and cats", priority: 5) }
+    let(:user_set) { FactoryBot.build(:user_set) }
     let(:serialized_user_set) { StoriesModerationSerializer.new(user_set).as_json }
 
     describe 'attributes' do
-      it 'renders the id field' do
-        expect(serialized_user_set).to have_key :id
+      it 'has the id field' do
+        expect(serialized_user_set[:id]).to eq user_set.id
       end
 
-      it 'renders the name field' do
-        expect(serialized_user_set).to have_key :name
+      it 'has the name field' do
+        expect(serialized_user_set[:name]).to eq user_set.name
       end
 
-      it 'renders the count field' do
-        expect(serialized_user_set).to have_key :count
+      it 'has the count field' do
+        expect(serialized_user_set[:count]).to eq user_set.count
       end
 
-      it 'renders the approved field' do
-        expect(serialized_user_set).to have_key :approved
+      it 'has the approved field' do
+        expect(serialized_user_set[:approved]).to eq user_set.approved
       end
 
-      it 'renders the approved field' do
-        expect(serialized_user_set).to have_key :featured
+      it 'has the approved field' do
+        expect(serialized_user_set[:featured]).to eq user_set.featured
       end
 
-      it 'renders the created_at field' do
-        expect(serialized_user_set).to have_key :created_at
+      it 'has the created_at field' do
+        expect(serialized_user_set[:created_at]).to eq user_set.created_at
       end
 
-      it 'renders the updated_at field' do
-        expect(serialized_user_set).to have_key :updated_at
+      it 'has the updated_at field' do
+        expect(serialized_user_set[:updated_at]).to eq user_set.updated_at
+      end
+
+      it 'has the featured_at field' do
+        expect(serialized_user_set[:featured_at]).to eq user_set.featured_at
+      end
+
+      it 'has the privacy field' do
+        expect(serialized_user_set[:privacy]).to eq user_set.privacy
       end
     end
   end

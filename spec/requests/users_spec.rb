203e2ab08b7e1242c7b7ff38f5a015455357852d
Edwin Rozario <edwin@boost.co.nz>
2021-02-19 14:58:15 +1300

    test(User): Request spec for create

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Users', type: :request do
   let(:user) { create(:user) }
 
   describe 'show' do
-    before { get "/v3/users/#{user.id}?api_key=#{admin.authentication_token}" }
+    before { get "/v3/users/#{user.id}.json?api_key=#{admin.authentication_token}" }
 
     it 'returns user info' do
       response_attributes = JSON.parse(response.body)['user']
 
       expect(response_attributes).to eq (
-        { 
+        {
           'id' => user.id.to_s,
           'name' => user.name,
           'username' => user.username,
@@ -22,14 +22,38 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+  describe 'create' do
+    before do
+      params = { user: { name: 'New api user' } }
+
+      post "/v3/users.json?api_key=#{admin.authentication_token}&#{params.to_query}"
+    end
+
+    it 'returns user info' do
+      response_attributes = JSON.parse(response.body)['user']
+
+      user = SupplejackApi::User.last
+
+      expect(response_attributes).to eq (
+        {
+          'id' => user.id.to_s,
+          'name' => 'New api user',
+          'username' => user.username,
+          'email' => user.email,
+          'api_key' => user.authentication_token
+        }
+      )
+    end
+  end
+
   describe 'update' do
-    before { put "/v3/users/#{user.id}?api_key=#{admin.authentication_token}&user[name]=NewName" }
+    before { put "/v3/users/#{user.id}.json?api_key=#{admin.authentication_token}&user[name]=NewName" }
 
     it 'returns user info of updated user' do
       response_attributes = JSON.parse(response.body)['user']
 
       expect(response_attributes).to eq (
-        { 
+        {
           'id' => user.id.to_s,
           'name' => 'NewName',
           'username' => user.username,
@@ -41,13 +65,13 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'delete' do
-    before { delete "/v3/users/#{user.id}?api_key=#{admin.authentication_token}" }
+    before { delete "/v3/users/#{user.id}.json?api_key=#{admin.authentication_token}" }
 
     it 'returns user info' do
       response_attributes = JSON.parse(response.body)['user']
 
       expect(response_attributes).to eq (
-        { 
+        {
           'id' => user.id.to_s,
           'name' => user.name,
           'username' => user.username,

9b90f7b5baf1b7d5423568d75c382f82a4b8ec4c
Edwin Rozario <edwin@boost.co.nz>
2021-03-17 10:07:32 +1300

    conf(RSpecs): formt is now documentation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
+  config.formatter = :documentation
   config.mock_with :rspec
   config.infer_spec_type_from_file_location!
   config.infer_base_class_for_anonymous_controllers = false

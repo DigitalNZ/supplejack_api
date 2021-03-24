02d34596e93a1cd900d0299d0f98b5ddb86bb0b1
Edwin Rozario <edwin@boost.co.nz>
2021-03-24 14:11:57 +1300

    test(Story Items Request): Specs for index and show

--- spec/requests/story_items_spec.rb
+++ spec/requests/story_items_spec.rb
@@ -15,12 +15,22 @@ RSpec.describe 'Story Items Endpoints', type: :request do
       end
     end
 
+    context 'when requesting with a invalid story id' do
+      before { get "/v3/stories/thisstoryiddontexist/items.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
+
+      it 'returns an error' do
+        response_attributes = JSON.parse(response.body)
+
+        expect(response_attributes).to eq ({ 'errors' => 'Story with provided Id thisstoryiddontexist not found' })
+      end
+    end
+
     context 'when requesting with a user_key' do
       before { get "/v3/stories/#{story.id}/items.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
 
       it 'returns story items for the user key' do
         response_attributes = JSON.parse(response.body)
-  
+
         expect(response_attributes).to eq (
           story.set_items.sort_by(&:position).map do |item|
             {
@@ -41,22 +51,44 @@ RSpec.describe 'Story Items Endpoints', type: :request do
   describe '#show' do
     let(:item) { story.set_items.first }
 
-    before { get "/v3/stories/#{story.id}/items/#{item.id.to_s}.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
+    context 'when item exists' do
+      before { get "/v3/stories/#{story.id}/items/#{item.id.to_s}.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
 
-    it 'returns story items for the user key' do
-      response_attributes = JSON.parse(response.body)
+      it 'returns story items for the user key' do
+        response_attributes = JSON.parse(response.body)
 
-      expect(response_attributes).to eq (
-        {
-          'record_id' => item.record_id,
-          'id' => item.id.to_s,
-          'position' => item.position,
-          'type' => item.type,
-          'sub_type' => item.sub_type,
-          'content' => DEFAULT_CONTENT_PRESENTER.call(item),
-          'meta' => { 'size' => item.meta[:size], 'is_cover' => (item.content[:image_url] == story.cover_thumbnail) }
-        }
-      )
+        expect(response_attributes).to eq (
+          {
+            'record_id' => item.record_id,
+            'id' => item.id.to_s,
+            'position' => item.position,
+            'type' => item.type,
+            'sub_type' => item.sub_type,
+            'content' => DEFAULT_CONTENT_PRESENTER.call(item),
+            'meta' => { 'size' => item.meta[:size], 'is_cover' => (item.content[:image_url] == story.cover_thumbnail) }
+          }
+        )
+      end
+    end
+
+    context 'when story dosent exist' do
+      before { get "/v3/stories/thisstoryiddontexist/items/#{item.id.to_s}.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
+
+      it 'returns an error' do
+        response_attributes = JSON.parse(response.body)
+
+        expect(response_attributes).to eq ({ 'errors' => 'Story with provided Id thisstoryiddontexist not found' })
+      end
+    end
+
+    context 'when story item dosent exist' do
+      before { get "/v3/stories/#{story.id}/items/fakeitemid.json?api_key=#{admin.authentication_token}&user_key=#{story.user.api_key}" }
+
+      it 'returns error' do
+        response_attributes = JSON.parse(response.body)
+
+        expect(response_attributes).to eq ({ 'errors' => "StoryItem with provided Id fakeitemid not found for Story with provided Story Id #{story.id}" })
+      end
     end
   end
 

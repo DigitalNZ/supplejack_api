05c21b2077ee99f3339cf3c0cce0c22e23dd0cfb
Richard Matthews <richard@boost.co.nz>
2021-05-13 15:33:54 +1200

    Remove unneeded size attributes and logging statements

--- lib/supplejack_api/index_processor.rb
+++ lib/supplejack_api/index_processor.rb
@@ -2,40 +2,14 @@
 
 module SupplejackApi
   class IndexProcessor
-    attr_accessor :size
-
-    def initialize(size = 1000)
-      @size = size
-    end
-
     # rubocop:disable Rails/Output
     def call
       p 'Looking for records to index..' unless Rails.env.test?
 
-      # mem = GetProcessMem.new
-      # puts "Before the mock indexing.. #{mem.mb}"
-
-      # 5.times do
-      #   SupplejackApi::Record.ready_for_indexing.where(status: 'active').count
-
-      #   p "I am indexing records ... "
-      # end
-
-      # puts "After the mock indexing ... #{mem.mb}"
-
-      mem = GetProcessMem.new
-      puts "Before the while loop #{mem.mb}"
-
       while SupplejackApi::Record.ready_for_indexing.where(status: 'active').count.positive?
-        # puts "Before the count #{mem.mb}"
-
         p "There are #{SupplejackApi::Record.ready_for_indexing.where(status: 'active').count} records to be indexed.." unless Rails.env.test?
-      
-        # puts "Before sending to the index job #{mem.mb}"
 
         BatchIndexRecords.new(SupplejackApi::Record.ready_for_indexing.where(status: 'active').limit(500)).call
-
-        # puts "After Batch Index Records #{mem.mb}"
       end
 
       p 'Looking for records to remove..' unless Rails.env.test?
--- lib/tasks/index_processor.rb
+++ lib/tasks/index_processor.rb
@@ -3,23 +3,15 @@
 require 'rake'
 
 namespace :index_processor do
-  task :run, [:batch_size] => [:environment] do |_, args|
-
-    mem = GetProcessMem.new
-    puts "Before the loop do #{mem.mb}"
-
+  task :run, :environment do
     loop do
-      puts "start the loop do #{mem.mb}"
       fork do
         SupplejackApi::IndexProcessor.new.call
       end
 
       Process.wait
 
-      puts "Process finished, sleeping.."
       sleep 30
     end
-
-    puts "After the loop do #{mem.mb}"
   end
 end

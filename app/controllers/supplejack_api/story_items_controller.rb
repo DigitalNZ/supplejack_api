8a2caafedf76dedaacdd665fdcf88c090d901e69
Edwin Rozario <edwin@boost.co.nz>
2021-05-25 16:10:22 +1200

    refactor(StoryItemsController): index
    Now uses StoryItemSerializer

--- app/controllers/supplejack_api/story_items_controller.rb
+++ app/controllers/supplejack_api/story_items_controller.rb
@@ -5,10 +5,14 @@ module SupplejackApi
     include Concerns::Stories
     include Concerns::StoryItemsControllerMetrics
 
+    before_action :story_user_check, except: :index
+    before_action :find_story, only: :index
     before_action :story_user_check, except: %i[create update destroy]
 
     def index
-      render_response(:story_items)
+      render json: @story.set_items,
+             each_serializer: StoryItemSerializer,
+             root: false
     end
 
     def show
@@ -25,6 +29,14 @@ module SupplejackApi
 
     def destroy
       render_response(:story_item)
+    end
+
+    private
+
+    def find_story
+      @story = SupplejackApi::UserSet.find_by_id(params[:story_id])
+
+      render_error_with(I18n.t('errors.story_not_found', id: params[:story_id]), :not_found) unless @story
     end
   end
 end

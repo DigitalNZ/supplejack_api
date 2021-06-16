4a9c2170decb58d3eb8bd035d0e75db38f59fc0c
Edwin Rozario <edwin@boost.co.nz>
2021-06-17 11:22:27 +1200

    test(Records): request spec for shoe

---
+++ spec/requests/records_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+RSpec.describe 'Records Endpoints', type: :request do
+  let(:user)   { create(:user) }
+  let(:record) { create(:record) }
+
+  describe '#index' do
+    context 'whithout any search params' do
+    end
+
+    context 'with search params' do
+    end
+  end
+
+  describe '#show' do
+    context 'when record id dosent exist' do
+      let(:record_id) { Faker::Lorem.word }
+
+      before { get "/v3/records/#{record_id}.json?api_key=#{user.authentication_token}" }
+
+      it 'returns record not found error' do
+        response_attributes = JSON.parse(response.body)
+
+        expect(response_attributes).to eq ({ 'errors' => I18n.t('errors.record_not_found', id: record_id) })
+      end
+    end
+
+    context 'when record id exists' do
+      before { get "/v3/records/#{record.record_id}.json?api_key=#{user.authentication_token}" }
+
+      it 'returns record' do
+        response_attributes = JSON.parse(response.body)
+
+        p record.updated_at.strftime('%y/%d/%m')
+
+        expect(response_attributes).to eq(
+          {
+            'record' => {
+              'address' => record.address,
+              'created_at' => record.created_at.strftime('%y/%d/%m'),
+              'email' => record.email,
+              'name' => record.name,
+              'updated_at' => record.created_at.as_json
+            }
+          }
+        )
+      end
+    end
+  end
+
+  describe '#multiple' do
+  end
+
+  describe '#more_like_this' do
+  end
+end

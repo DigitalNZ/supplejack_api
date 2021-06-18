5bbd46ab589307b33c37d7ee1f558ccd29b5e342
Edwin Rozario <edwin@boost.co.nz>
2021-06-18 16:18:23 +1200

    style(Rubocop): fixes for concepts_controller_spec

--- spec/controllers/supplejack_api/concepts_controller_spec.rb
+++ spec/controllers/supplejack_api/concepts_controller_spec.rb
@@ -9,30 +9,34 @@ module SupplejackApi
     before { @user = FactoryBot.create(:user, authentication_token: 'apikey', role: 'developer') }
 
     describe 'GET show' do
-      before {
+      before do
         @concept = FactoryBot.create(:concept)
         allow(controller).to receive(:current_user) { @user }
-      }
+      end
 
       it 'should find the concept and assign it' do
         expect(Concept).to receive(:custom_find).with('123', @user, nil).and_return(@concept)
-        get :show, params: { id: 123, search: {}, api_key: 'abc123' }, format: "json"
+        get :show, params: { id: 123, search: {}, api_key: 'abc123' }, format: 'json'
         expect(assigns(:concept)).to eq(@concept)
       end
 
       it 'renders a error when records is not found' do
-        allow(Concept).to receive(:custom_find).and_raise(Mongoid::Errors::DocumentNotFound.new(Concept, ['123'], ['123']))
-        get :show, params: { id: 123, search: {}, api_key: 'abc123' }, :format => 'json'
-        expect(response.body).to eq({:errors => 'Concept with ID 123 was not found'}.to_json)
+        allow(Concept).to receive(:custom_find).and_raise(
+          Mongoid::Errors::DocumentNotFound.new(Concept, ['123'], ['123'])
+        )
+
+        get :show, params: { id: 123, search: {}, api_key: 'abc123' }, format: 'json'
+
+        expect(response.body).to eq({ errors: 'Concept with ID 123 was not found' }.to_json)
       end
     end
 
     describe 'GET index' do
-      before {
+      before do
         @search = ConceptSearch.new
         allow(@search).to receive(:valid?) { false }
         allow(@search).to receive(:new) { @search }
-      }
+      end
 
       it 'initializes a new search instance' do
         expect(ConceptSearch).to receive(:new).with(hash_including(text: 'dogs')).and_return(@search)
@@ -42,13 +46,13 @@ module SupplejackApi
 
       it 'sets the request url on search object' do
         allow_any_instance_of(ConceptSearch).to receive(:valid?) { false }
-        get :index, params: { api_key: 'apikey' }, format: "json"
+        get :index, params: { api_key: 'apikey' }, format: 'json'
         expect(assigns(:search).request_url).to eq 'http://test.host/concepts?api_key=apikey'
       end
 
       it 'should set the current_user on the search' do
         allow_any_instance_of(ConceptSearch).to receive(:valid?) { false }
-        get :index, params: { api_key: 'apikey' }, format: "json"
+        get :index, params: { api_key: 'apikey' }, format: 'json'
         expect(assigns(:search).scope).to eq @user
       end
 
@@ -59,18 +63,21 @@ module SupplejackApi
       end
 
       it 'renders a error when the requested field doesn\'t exist' do
-        allow(SearchSerializer).to receive(:new).and_raise(Sunspot::UnrecognizedFieldError.new('No field configured for Concept with name "something"'))
+        allow(SearchSerializer).to receive(:new).and_raise(
+          Sunspot::UnrecognizedFieldError.new('No field configured for Concept with name "something"')
+        )
+
         get :index, params: { api_key: 'apikey', and: { something: true } }, format: 'json'
+
         expect(response.body).to eq({ errors: 'No field configured for Concept with name "something"' }.to_json)
       end
 
       it 'should return an error if the search request is invalid' do
         allow_any_instance_of(ConceptSearch).to receive(:valid?) { false }
         allow_any_instance_of(ConceptSearch).to receive(:errors) { ['The page parameter can not exceed 100,000'] }
-        get :index, params: { api_key: 'apikey', page: 100001 }, format: 'json'
+        get :index, params: { api_key: 'apikey', page: 100_001 }, format: 'json'
         expect(response.body).to eq({ errors: ['The page parameter can not exceed 100,000'] }.to_json)
       end
     end
-
   end
 end

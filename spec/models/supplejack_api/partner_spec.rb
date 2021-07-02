b5dd24549901f9c95e5ea58a2d74a6de2458d655
Edwin Rozario <rozarioed@gmail.com>
2021-07-02 15:51:23 +1200

    style(Rubocop): fixes for models/supplejack_api/partner_spec

--- spec/models/supplejack_api/partner_spec.rb
+++ spec/models/supplejack_api/partner_spec.rb
@@ -6,10 +6,8 @@ module SupplejackApi
   describe Partner do
     describe 'validations' do
       it 'is not valid without a name' do
-        partner = Partner.new()
-        expect(partner.valid?).to be_falsey
+        expect(Partner.new.valid?).to be_falsey
       end
     end
   end
-
 end

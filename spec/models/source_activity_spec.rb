32cbdc193a56c130a6e70546f9c98b8d423a7290
Edwin Rozario <rozarioed@gmail.com>
2021-07-02 15:27:03 +1200

    style(Rubocop): fixes for models/source_activity_spec

--- spec/models/source_activity_spec.rb
+++ spec/models/source_activity_spec.rb
@@ -3,49 +3,49 @@
 require 'spec_helper'
 
 module SupplejackApi
-	describe SourceActivity do
-		describe '#increment' do
-			context 'SourceActivity doesn\'t exist' do
-				it 'creates a SourceActivity with source activity of 1' do
-					SupplejackApi::SourceActivity.increment
-					expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 1
-				end	
-			end
+  describe SourceActivity do
+    describe '#increment' do
+      context 'SourceActivity doesn\'t exist' do
+        it 'creates a SourceActivity with source activity of 1' do
+          SupplejackApi::SourceActivity.increment
+          expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 1
+        end
+      end
 
-			context 'SourceActivity exists' do
-				before {
-					SupplejackApi::SourceActivity.create(source_clicks: 12)
-				}
+      context 'SourceActivity exists' do
+        before { SupplejackApi::SourceActivity.create(source_clicks: 12) }
 
-				it 'increments source activity' do
-					SupplejackApi::SourceActivity.increment
-					expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 13
-				end	
-			end
-		end
+        it 'increments source activity' do
+          SupplejackApi::SourceActivity.increment
 
-		describe '#get_source_clicks' do
-			it 'returns the source_clicks count if SourceActive exists' do
-				SupplejackApi::SourceActivity.increment
-				expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 1
-			end
+          expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 13
+        end
+      end
+    end
 
-			it 'returns nil if no SourceActivity' do
-				expect(SupplejackApi::SourceActivity.get_source_clicks).to be_nil
-			end
-		end
+    describe '#get_source_clicks' do
+      it 'returns the source_clicks count if SourceActive exists' do
+        SupplejackApi::SourceActivity.increment
 
-		describe '#reset' do
-			it 'deletes first instance of SourceActivity if exists' do
-				SupplejackApi::SourceActivity.increment
-				SupplejackApi::SourceActivity.reset
-				expect(SupplejackApi::SourceActivity.count).to be 0
-			end
+        expect(SupplejackApi::SourceActivity.get_source_clicks).to eq 1
+      end
 
-			it 'handles no source activity' do
-				2.times { SupplejackApi::SourceActivity.reset }
-			end
-		end
+      it 'returns nil if no SourceActivity' do
+        expect(SupplejackApi::SourceActivity.get_source_clicks).to be_nil
+      end
+    end
 
-	end
+    describe '#reset' do
+      it 'deletes first instance of SourceActivity if exists' do
+        SupplejackApi::SourceActivity.increment
+        SupplejackApi::SourceActivity.reset
+
+        expect(SupplejackApi::SourceActivity.count).to be 0
+      end
+
+      it 'handles no source activity' do
+        2.times { SupplejackApi::SourceActivity.reset }
+      end
+    end
+  end
 end

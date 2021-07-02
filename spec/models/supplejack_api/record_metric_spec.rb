643bfa2768653f8823fd2449577f2a6672de5597
Edwin Rozario <rozarioed@gmail.com>
2021-07-02 15:54:08 +1200

    style(Rubocop): fixes for models/supplejack_api/record_metric_spec

--- spec/models/supplejack_api/record_metric_spec.rb
+++ spec/models/supplejack_api/record_metric_spec.rb
@@ -74,14 +74,18 @@ RSpec.describe SupplejackApi::RecordMetric do
   end
 
   describe '::spawn' do
-    let!(:record_metric) { create(:record_metric, record_id: 1, page_views: 1, display_collection: 'NDHA', date: Time.now.yesterday) }
+    let!(:record_metric) do
+      create(:record_metric, record_id: 1, page_views: 1, display_collection: 'NDHA', date: Time.current.yesterday)
+    end
 
     it 'creates a new RecordMetric when there is not one for the provided day and record_id' do
-      expect { SupplejackApi::RecordMetric.spawn(2, { 'appeared_in_searches' => 1 }, 'NDHA') }.to change(SupplejackApi::RecordMetric, :count).by(1)
+      expect { SupplejackApi::RecordMetric.spawn(2, { 'appeared_in_searches' => 1 }, 'NDHA') }
+        .to change(SupplejackApi::RecordMetric, :count).by(1)
     end
 
     it 'does not create a RecordMetric when the provided day and record_id allready exist' do
-      expect { SupplejackApi::RecordMetric.spawn(record_metric.record_id, { 'appeared_in_searches' => 1 }, 'NDHA') }.to change(SupplejackApi::RecordMetric, :count).by(0)
+      expect { SupplejackApi::RecordMetric.spawn(record_metric.record_id, { 'appeared_in_searches' => 1 }, 'NDHA') }
+        .to change(SupplejackApi::RecordMetric, :count).by(0)
     end
 
     it 'increments an existing Record Metric' do
@@ -91,7 +95,10 @@ RSpec.describe SupplejackApi::RecordMetric do
     end
 
     it 'increments an existing Record Metric by a given hash of metrics' do
-      SupplejackApi::RecordMetric.spawn(record_metric.record_id, { 'page_views' => 7, 'appeared_in_searches' => 6, 'user_set_views' => 10 }, 'NDHA')
+      SupplejackApi::RecordMetric.spawn(
+        record_metric.record_id, { 'page_views' => 7, 'appeared_in_searches' => 6, 'user_set_views' => 10 }, 'NDHA'
+      )
+
       record_metric.reload
 
       expect(record_metric.page_views).to eq 8

de1335135d9e52ab241bbdcc6076e9fee9d79c76
Edwin Rozario <rozarioed@gmail.com>
2021-07-05 16:34:11 +1200

    style(Rubocop): fixes spec/helpers

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-  describe SupplejackApi::ApplicationHelper do
+describe SupplejackApi::ApplicationHelper do
+  def label(string)
+    safe_join([string, tag(:span)])
+  end
 
-    def label(string)
-      safe_join([string, content_tag(:span)])
-    end
-
-    describe '#extract_sort_info' do
-      it 'returns the column and direction from the order param' do
-        allow(helper).to receive(:params) { {order: 'name_asc'} }
-        expect(helper.extract_sort_info).to eq ['name', 'asc']
-      end
+  describe '#extract_sort_info' do
+    it 'returns the column and direction from the order param' do
+      allow(helper).to receive(:params) { { order: 'name_asc' } }
+      expect(helper.extract_sort_info).to eq %w[name asc]
     end
   end
+end

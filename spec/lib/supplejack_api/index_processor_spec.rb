11852b995dd10ffa2a0afd77ac5dbb40c2014646
Edwin Rozario <rozarioed@gmail.com>
2021-07-06 14:04:00 +1200

    style(Rubocop): fixes spec/lib/*

--- spec/lib/supplejack_api/index_processor_spec.rb
+++ spec/lib/supplejack_api/index_processor_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe SupplejackApi::IndexProcessor do
   let(:active_records) { create_list(:record_with_fragment, 5, :ready_for_indexing) }
-  let(:deleted_records) { create_list(:record_with_fragment, 5, :ready_for_indexing, :deleted)}
-  let(:index_processor)      { SupplejackApi::IndexProcessor.new }
+  let(:deleted_records) { create_list(:record_with_fragment, 5, :ready_for_indexing, :deleted) }
+  let(:index_processor) { SupplejackApi::IndexProcessor.new }
 
   describe '#call' do
     it 'indexes records in batches of the provided size' do

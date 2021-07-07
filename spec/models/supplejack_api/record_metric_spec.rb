c36fd62992c3f9558580296eb5c7b27823d1ad9f
Edwin Rozario <rozarioed@gmail.com>
2021-07-07 13:54:24 +1200

    fix(SupplejackApi::RecordMetric): spec

--- spec/models/supplejack_api/record_metric_spec.rb
+++ spec/models/supplejack_api/record_metric_spec.rb
@@ -75,7 +75,10 @@ RSpec.describe SupplejackApi::RecordMetric do
 
   describe '::spawn' do
     let!(:record_metric) do
-      create(:record_metric, record_id: 1, page_views: 1, display_collection: 'NDHA', date: Time.current.yesterday)
+      create(:record_metric, record_id: 1,
+                             page_views: 1,
+                             display_collection: 'NDHA',
+                             date: Time.current.yesterday)
     end
 
     it 'creates a new RecordMetric when there is not one for the provided day and record_id' do
@@ -95,9 +98,11 @@ RSpec.describe SupplejackApi::RecordMetric do
     end
 
     it 'increments an existing Record Metric by a given hash of metrics' do
-      SupplejackApi::RecordMetric.spawn(
-        record_metric.record_id, { 'page_views' => 7, 'appeared_in_searches' => 6, 'user_set_views' => 10 }, 'NDHA'
-      )
+      SupplejackApi::RecordMetric.spawn(record_metric.record_id,
+                                        { 'page_views' => 7,
+                                          'appeared_in_searches' => 6,
+                                          'user_set_views' => 10 },
+                                        'NDHA')
 
       record_metric.reload
 

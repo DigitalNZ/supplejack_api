77488917b662662f11278c682fe2096e294f376a
Richard Matthews <richard@boost.co.nz>
2021-10-20 14:32:11 +1300

    rubocopping

--- app/controllers/supplejack_api/concerns/stories/multiple.rb
+++ app/controllers/supplejack_api/concerns/stories/multiple.rb
@@ -8,57 +8,57 @@ module SupplejackApi
           stories = multiple_stories_params['stories'].each_with_object([]) do |story, stories_array|
             set = SupplejackApi::UserSet.custom_find(story['id'])
             return render_error_with(I18n.t('errors.story_not_found', id: story['id']), :not_found) unless set
-    
+
             authorize(set)
             stories_array.push(set)
           end
-    
+
           changes = multiple_stories_params['stories'].each_with_object([]) do |story_params, changes_array|
             set = stories.find { |s| s.id.to_s == story_params['id'] }
-    
+
             item_ids = story_params['items'].each_with_object([]) do |item, ids|
               item = set.set_items.build(item)
-    
+
               return render_error_with(item.errors.messages.values.join(', '), :bad_request) unless item.valid?
-    
+
               ids.push(item.id)
             end
-    
+
             set.save!
-    
+
             changes_array.push({
-                                story_id: story_params['id'],
-                                item_ids: item_ids
-                              })
+                                 story_id: story_params['id'],
+                                 item_ids: item_ids
+                               })
           end
-    
+
           render json: changes
         end
-    
+
         def multiple_remove
           stories = multiple_stories_params['stories'].each_with_object([]) do |story, stories_array|
             set = SupplejackApi::UserSet.custom_find(story['id'])
             return render_error_with(I18n.t('errors.story_not_found', id: story['id']), :not_found) unless set
-    
+
             authorize(set)
             stories_array.push(set)
           end
-    
+
           multiple_stories_params['stories'].each do |story_params|
             set = stories.find { |s| s.id.to_s == story_params['id'] }
-    
+
             story_params['items'].each do |item|
               set_item = set.set_items.find_by_id(item[:id])
-    
+
               return render json: { errors: I18n.t('errors.record_not_found', id: item[:id]) },
                             status: :not_found unless set_item
-    
+
               set_item.destroy!
             end
-    
+
             set.save!
           end
-    
+
           head :no_content
         end
       end

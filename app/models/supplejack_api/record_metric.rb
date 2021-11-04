50a3aaa7f5390684a3e1c2fa58ad5480747689ea
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2021-11-04 23:40:43 +0100

    fix: collection.update_one is faster

--- app/models/supplejack_api/record_metric.rb
+++ app/models/supplejack_api/record_metric.rb
@@ -46,11 +46,13 @@ module SupplejackApi
 
     def self.spawn(record_id, metrics, display_collection, date = Time.now.utc.beginning_of_day.yesterday)
       return unless SupplejackApi.config.log_metrics == true
+      return if record_id.nil? || display_collection.nil?
 
-      record = where(record_id: record_id, date: date).first
-      record = new(record_id: record_id, date: date, display_collection: display_collection) if record.nil?
-      record.inc(metrics)
-      record.save
+      collection.update_one(
+        { record_id: record_id, date: date.to_date, display_collection: display_collection },
+        { '$inc' => metrics },
+        upsert: true
+      )
     end
   end
 end

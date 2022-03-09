e5b94afb444a131d0d1cf9014068493da9189242
Edwin Rozario <rozarioed@gmail.com>
2022-03-10 11:39:27 +1300

    fix(IndexProcessor): Race condition
    If a second can be broken down to 3 parts and if the following happens in those 3 parts sequentially.
    - Enrichment 1 updates record x
    - IndexProcessor pics up the record x
    - Enrichment 2 updates record x
    The second update wont be captured on indexing as the updated_at for both enrichments are at the same second.
    So if we query ready_for_indexing with a delay of 5 seconds we make sure that both updates written in any second is captured.

--- lib/supplejack_api/index_processor.rb
+++ lib/supplejack_api/index_processor.rb
@@ -2,30 +2,56 @@
 
 module SupplejackApi
   class IndexProcessor
-    # rubocop:disable Rails/Output
     def call
+      index_available_records
+
+      unindex_available_records
+    end
+
+    # rubocop:disable Rails/Output
+    def index_available_records
       p 'Looking for records to index..' unless Rails.env.test?
 
-      while SupplejackApi::Record.ready_for_indexing.where(status: 'active').count.positive?
+      available_records = records_ready_5_secods_ago
 
-        records = SupplejackApi::Record.ready_for_indexing.where(status: 'active').limit(500).to_a
+      while available_records.count.positive?
+        records = available_records.limit(500).to_a
 
         p "[#{Time.current}] There are #{records.count} records to be indexed.." unless Rails.env.test?
 
         BatchIndexRecords.new(records).call
-      end
 
+        available_records = records_ready_5_secods_ago
+      end
+    end
+
+    def unindex_available_records
       p 'Looking for records to unindex..' unless Rails.env.test?
 
-      while SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active').count.positive?
+      available_records = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active')
 
-        records = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active').limit(500).to_a
+      while available_records.count.positive?
+        records = available_records.limit(500).to_a
 
         p "[#{Time.current}] There are #{records.count} records to be removed from the index.." unless Rails.env.test?
 
         BatchRemoveRecordsFromIndex.new(records).call
+
+        available_records = SupplejackApi::Record.ready_for_indexing.where(:status.ne => 'active')
       end
     end
     # rubocop:enable Rails/Output
+
+    # This query is made to counter a race condition.
+    # If a second can be broken down to 3 parts and if the following happens in those 3 parts sequentially.
+    # - Enrichment 1 updates record x
+    # - IndexProcessor pics up the record x
+    # - Enrichment 2 updates record x
+    # The second update wont be captured on indexing as the updated_at for both enrichments are at the same second.
+    # So if we query ready_for_indexing with a delay of 5 seconds we make sure that
+    # both updates written in any second is captured
+    def records_ready_5_secods_ago
+      SupplejackApi::Record.ready_for_indexing.where(status: 'active', :updated_at.gt => (Time.current - 5.seconds))
+    end
   end
 end

28c14ac51332e3448be667e2fe2a7613e238e623
Richard Matthews <richard@boost.co.nz>
2023-02-20 15:20:27 +1300

    Removing story code

--- spec/factories/user_sets.rb
+++ spec/factories/user_sets.rb
@@ -1,46 +1,46 @@
 # frozen_string_literal: true
 
 module SupplejackApi
-  FactoryBot.define do
-    factory :user_set, class: SupplejackApi::UserSet do
-      association :record, factory: :record_with_fragment
-      association :user, factory: :user
+  # FactoryBot.define do
+  #   factory :user_set, class: SupplejackApi::UserSet do
+  #     association :record, factory: :record_with_fragment
+  #     association :user, factory: :user
 
-      name             { Faker::Movie.title }
-      description      { Faker::Movie.quote }
-      copyright        { 0 }
-      url              { Faker::Internet.url }
-      priority         { 0 }
-      count_updated_at { Time.zone.today }
-      subjects         { [Faker::Verb.base] }
-      approved         { false }
-      featured         { false }
-      featured_at      { Time.zone.today }
-      cover_thumbnail  { Faker::Internet.url }
-      username         { nil }
-      email            { Faker::Internet.email }
+  #     name             { Faker::Movie.title }
+  #     description      { Faker::Movie.quote }
+  #     copyright        { 0 }
+  #     url              { Faker::Internet.url }
+  #     priority         { 0 }
+  #     count_updated_at { Time.zone.today }
+  #     subjects         { [Faker::Verb.base] }
+  #     approved         { false }
+  #     featured         { false }
+  #     featured_at      { Time.zone.today }
+  #     cover_thumbnail  { Faker::Internet.url }
+  #     username         { nil }
+  #     email            { Faker::Internet.email }
 
-      factory :user_set_with_set_item do
-        after(:create) do |user_set|
-          # This is needed to reset the memoized instance variable
-          # @records that has been set in user_set
-          # Without it you will not have set_items
-          user_set.instance_variable_set(:@records, nil)
-          create(:set_item,
-                 type: 'embed',
-                 sub_type: 'record',
-                 content: { id: user_set.record.record_id },
-                 user_set: user_set)
-        end
-      end
-    end
+  #     factory :user_set_with_set_item do
+  #       after(:create) do |user_set|
+  #         # This is needed to reset the memoized instance variable
+  #         # @records that has been set in user_set
+  #         # Without it you will not have set_items
+  #         user_set.instance_variable_set(:@records, nil)
+  #         create(:set_item,
+  #                type: 'embed',
+  #                sub_type: 'record',
+  #                content: { id: user_set.record.record_id },
+  #                user_set: user_set)
+  #       end
+  #     end
+  #   end
 
-    factory :set_item, class: SupplejackApi::SetItem do
-      before(:create) do |set_item|
-        record = create(:record_with_fragment)
-        set_item.record_id = record.record_id
-      end
-      sequence(:position)
-    end
-  end
+  #   factory :set_item, class: SupplejackApi::SetItem do
+  #     before(:create) do |set_item|
+  #       record = create(:record_with_fragment)
+  #       set_item.record_id = record.record_id
+  #     end
+  #     sequence(:position)
+  #   end
+  # end
 end

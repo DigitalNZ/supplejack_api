5c9a4ce6f76908c8a7cd2492fe7750b1c9d1a7b0
Richard Matthews <richard@boost.co.nz>
2023-03-15 16:48:59 +1300

    Pull the facet method from the Schema and add it to the query

--- app/solr_queries/query_builder/facets.rb
+++ app/solr_queries/query_builder/facets.rb
@@ -27,6 +27,17 @@ module QueryBuilder
       search.build do
         facet_list.each do |facet_name|
           facet(facet_name, limit: facets_per_page, offset: facets_offset)
+
+          adjust_solr_params do |params|
+            facet_field_name = facet_field_name = params[:"facet.field"].last
+            field_definition = RecordSchema.fields[facet_name]
+
+            next unless field_definition.facet_method.present?
+
+            indexed_name = params[:'facet.field'].find { |facet| facet.include? facet_name.to_s }
+         
+            params[:"f.#{indexed_name}.method"] = field_definition.facet_method
+          end
         end
       end
     end

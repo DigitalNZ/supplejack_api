83b6b4da3784e88a8e40f26e2e1e137fbe37f0fe
Richard Matthews <rj.matthews23@gmail.com>
2025-01-21 09:15:34 +1300

    Bundle audit and rubocop

--- lib/supplejack_api/index_processor.rb
+++ lib/supplejack_api/index_processor.rb
@@ -10,7 +10,7 @@ module SupplejackApi
       log('Looking for records...')
 
       Mongoid::QueryCache.clear_cache
-      
+
       index_available_records
       unindex_available_records
     end

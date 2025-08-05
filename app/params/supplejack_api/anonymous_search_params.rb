202a1acc47450cfee5e64a77f99db6b0afd67836
Tim Wright <tim@Tims-MacBook-Pro.local>
2025-08-05 13:54:10 +1200

    Added missing params

--- app/params/supplejack_api/anonymous_search_params.rb
+++ app/params/supplejack_api/anonymous_search_params.rb
@@ -4,7 +4,9 @@ module SupplejackApi
   class AnonymousSearchParams < SearchParams
     self.max_values = {
       page: 100,
-      per_page: 100
+      per_page: 100,
+      facets_per_page: 350,
+      facets_page: 5000
     }
   end
 end
